73d7b4398112c321c11c226456371296abf929cf
emaalk <emaalk@bu.edu>
2024-02-24 21:54:51 +0200

    Signed-off-by: emaalk <emaalk@bu.edu>
    Added more logs.
    Set implicit wait time for selenium.

--- subscribe.py
+++ subscribe.py
@@ -8,12 +8,13 @@ from time import sleep
 from datetime import datetime
 
 URL_GRAND_KENYON_HAIFA="https://www.holmesplace.co.il/place-studio/?club=202"
-CLASS="פילאטיס"
+CLASS="ספינינג"
 PREF_SEATS =[39, 36, 35, 38, 27]
 DATE=datetime.now()
+LOG_PATH="/tmp/subscribe_log_%s.txt"%DATE
 
 def log(text):
-    text_file = open("/tmp/subscribe_log_%s.txt"%DATE, "a")
+    text_file = open(LOG_PATH, "a")
     text_file.write("%s\n"%text)
     text_file.close()
     print(text)
@@ -33,18 +34,17 @@ parser.add_argument('-n','--number', help='phone number to access account', requ
 parser.add_argument('-p','--password', help='password to access account', required=True)
 args = parser.parse_args()
 
-log("Script starts to run at %s, subscribe to %s"%(datetime.now(), CLASS)) 
+log("Script starts to run at %s, subscribe to %s. Logs are located at %s"%(datetime.now(), CLASS, LOG_PATH)) 
 options = webdriver.ChromeOptions() 
 options.add_argument("--start-maximized") 
 options.add_argument('--log-level=3')
 
 log("Creating Chrome driver")
 driver = webdriver.Chrome(executable_path="chromedriver", chrome_options=options)
+driver.implicitly_wait(10)
 
 log("Accessing %s"%URL_GRAND_KENYON_HAIFA)
 driver.get(URL_GRAND_KENYON_HAIFA)
-
-sleep(10)
 
 remove_add(driver)
 
@@ -53,8 +53,6 @@ access_login = driver.find_element(By.LINK_TEXT, "כניסה / הרשמה")
 access_login.click()
 
 remove_add(driver)
-
-sleep(5)
 
 log("inserting password")
 password = driver.find_element(By.ID, "LoginPassword")
@@ -66,14 +64,10 @@ phone = driver.find_element(By.ID, "LoginPhone")
 phone.clear()
 phone.send_keys(args.number)
 
-sleep(5)
-
 remove_add(driver)
 
 login = driver.find_element(By.ID, "loginButton")
 login.click()
-
-sleep(10)
 
 register = driver.find_elements_by_link_text('בחר מקום')
 if not (len(register)):
@@ -86,32 +80,30 @@ for class_idx,class_info in enumerate(register):
         continue
     class_name = class_str.partition("data-lessonname=\"")[2].split("\" ")[0]
     class_date = class_str.partition("data-date=\"")[2].split("\" ")[0]
+    class_time = class_str.partition("data-time=\"")[2].split("\" ")[0]
     class_teacher = class_str.partition("data-teacher=\"")[2].split("\" ")[0]
-    log("Class number %d name:%s date:%s teacher:%s"%(class_idx, class_name, class_date, class_teacher))
+    log("Class number %d name:%s date:%s time:%s teacher:%s"%(class_idx, class_name, class_date, class_time, class_teacher))
     
     if CLASS in class_info.get_attribute('outerHTML'):
         log(class_info.get_attribute('outerHTML'))
-        sleep(5)
+        sleep(10)
         class_info.click()
-        sleep(5)
         seats= driver.find_element(By.ID, "availableSeatsSELECT")
-        sleep(5)
         drop=Select(seats)
         pref_seat_choosen = False
         for pref_seat in PREF_SEATS:
             try:
                 drop.select_by_visible_text(str(pref_seat))
-                pref_seat_choosen = True   
+                pref_seat_choosen = True
+                print("Prefered seat %d was choosed"%pref_seat)
+                break
             except:
                 log("Seat %d is not available"%pref_seat)
         if not pref_seat_choosen:
             log("A default seat was choosen")
-        sleep(10)
         register = driver.find_element_by_link_text('הזמנה')
-        sleep(5)
         register.click()
-        log("registered successfullyo to the class")
-        sleep(5)
+        log("registered successfully to the class")
 
 log("Close Chrome driver")
 driver.close()
